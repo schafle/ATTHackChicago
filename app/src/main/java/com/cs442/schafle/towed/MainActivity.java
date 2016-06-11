14d766bf78b38203fe24ff26a6830c83d5660c12
wtpoo <surajchafale@gmail.com>
2016-06-11 11:06:38 -0500

    UI refactored, Fancy button added, Made the string invisible on create, changed app icon

--- app/src/main/java/com/cs442/schafle/towed/MainActivity.java
+++ app/src/main/java/com/cs442/schafle/towed/MainActivity.java
@@ -1,19 +1,20 @@
 package com.cs442.schafle.towed;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import mehdi.sakout.fancybuttons.FancyButton;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText vehicleNumber;
     TextView display_text;
     String vehicleNumberValue = "";
-    Button btnSubmit;
+    FancyButton btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +22,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         vehicleNumber = (EditText) findViewById(R.id.vehicle_number);
         display_text = (TextView) findViewById(R.id.text12);
-        btnSubmit = (Button) findViewById(R.id.btnSubmit);
-
+        btnSubmit = (FancyButton) findViewById(R.id.btnSubmit);
+        display_text.setVisibility(View.INVISIBLE);
         //OnCLick Listener Event
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "Submitted successfully!", Toast.LENGTH_SHORT).show();
                 display_text.setText(vehicleNumber.getText());           //Resetting the form
+                vehicleNumber.setVisibility(View.INVISIBLE);
+                btnSubmit.setVisibility(View.INVISIBLE);
+                display_text.setVisibility(View.VISIBLE);
             }
         });
     }
